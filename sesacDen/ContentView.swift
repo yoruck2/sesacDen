ea79b41b072187729f5a39eeb087c83ee3011303
seunghyun Lee <yoruck2@gmail.com>
2024-10-08 14:16:58 +0900

    init

---
+++ sesacDen/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  sesacDen
+//
+//  Created by dopamint on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ sesacDen/sesacDenApp.swift
@@ -0,0 +1,17 @@
+//
+//  sesacDenApp.swift
+//  sesacDen
+//
+//  Created by dopamint on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct sesacDenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
